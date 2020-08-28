cc02471ed22f1a6de51a54752a9c31f3ae7c9248
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-08-27 23:10:06 -0400

    started over bc I deleted componenets on accident

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {ProductContext} from "./contexts/allContexts";
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+export const Product
+
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState([products]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		//setCart(cart.push(item));
+		//setCart(item);
+		cart.push(item);
+
+		console.log("cart: ", cart, "item: ", item);
 	};
 
 	return (
 		<div className="App">
+			
 			<Navigation cart={cart} />
 
 			{/* Routes */}
